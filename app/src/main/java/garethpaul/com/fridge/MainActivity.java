f2e2eb58714cb9c427dcad04e46247b2cc0b6e4c
Gareth Paul Jones <gareth@garethpaul.com>
2015-05-10 10:40:33 -0700

    adding listview to mainactivity

--- app/src/main/java/garethpaul/com/fridge/MainActivity.java
+++ app/src/main/java/garethpaul/com/fridge/MainActivity.java
@@ -1,19 +1,89 @@
 package garethpaul.com.fridge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        // Add ListView
+        lvItems = (ListView) findViewById(R.id.listView);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+
+        // Remove warnings
+        EditText etNewItem = (EditText) findViewById(R.id.editText);
+        etNewItem.requestFocus();
+        InputMethodManager inputManager = (InputMethodManager)this.getSystemService(INPUT_METHOD_SERVICE);
+        inputManager.restartInput(etNewItem);
+
+        // SetupListView
+        setupListViewListener();
+
     }
 
+    // Attaches a long click listener to the ListView
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.editText);
+        String itemText = etNewItem.getText().toString();
+
+        // Add the item to the listview
+        itemsAdapter.add(itemText);
+
+        // Set the text to empty
+        etNewItem.setText("");
+
+        // Hide the keyboard
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(etNewItem.getWindowToken(), 0);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
