6aeee653d1f29a37d1063edecb02dd3b3107e4d1
Gareth Jones <gareth@garethpaul.com>
2026-06-19 14:42:48 -0700

    test: isolate host filesystem harness

--- scripts/test-item-store.sh
+++ scripts/test-item-store.sh
@@ -10,7 +10,7 @@ javac -source 1.7 -target 1.7 -d "$BUILD_DIR" \
   "$ROOT_DIR/app/src/main/java/garethpaul/com/fridge/ItemStore.java" \
   "$ROOT_DIR/app/src/main/java/garethpaul/com/fridge/ItemFileTransaction.java" \
   "$ROOT_DIR/app/src/main/java/garethpaul/com/fridge/ItemListTransaction.java" \
-  "$ROOT_DIR/app/src/test/java/garethpaul/com/fridge/FileInputStreamCompat.java" \
-  "$ROOT_DIR/app/src/test/java/garethpaul/com/fridge/ItemStoreHostTest.java"
+  "$ROOT_DIR/scripts/host-tests/garethpaul/com/fridge/FileInputStreamCompat.java" \
+  "$ROOT_DIR/scripts/host-tests/garethpaul/com/fridge/ItemStoreHostTest.java"
 
 java -cp "$BUILD_DIR" garethpaul.com.fridge.ItemStoreHostTest
